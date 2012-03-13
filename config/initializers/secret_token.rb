25c5cd17ef88e41b3a4cacc4eadfb6c97ce7f9ec
Nick Hengeveld <nickh@github.com>
2012-03-13 15:26:53 -0700

    Created new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+K9walker::Application.config.secret_token = '00881bfe82cede55e98b746b137c9ff634298b8b80a128da2e84e0c2ece1174d0d643003d6436fb34146d96089529e5d7b73bdcd1464244781575ca454bfc854'
