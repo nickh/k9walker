f93081be1de34bb93743491c56d38cc30da3ce5a
Nick Hengeveld <nickh@github.com>
2012-03-13 15:33:06 -0700

    Updated list of dog breeds

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def dog_breeds
+    [
+      'Chihuahua',
+      'Border Collie',
+      'Dalmatian',
+      'German Shepherd',
+      'Lab',
+      'Rottweiler',
+      'Shih Tzu'
+    ]
+  end
 end
